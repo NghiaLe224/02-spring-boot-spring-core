4fb97c7719d966d7aa4d013e460d0bdf79471e6c
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-05-16 12:02:05 +0700

    update Bean Scope

--- src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -1,9 +1,12 @@
 package com.luv2code.springcoredemo.common;
 
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 public class BaseballCoach implements Coach{
 
--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     private Coach myCoach;
 
+    private Coach anotherCoach;
+
 //    @Autowired
 //    public DemoController(@Qualifier("trackCoach") Coach coach){
 //        this.myCoach = coach;
@@ -21,13 +23,22 @@ public class DemoController {
 //    }
 
     @Autowired
-    public DemoController(@Qualifier("baseballCoach") Coach coach){
+    public DemoController(@Qualifier("baseballCoach") Coach coach,
+                          @Qualifier("baseballCoach") Coach anotherCoach){
         System.out.println("In Constructor: " + this.getClass().getSimpleName());
         this.myCoach = coach;
+        this.anotherCoach = anotherCoach;
+        System.out.println("same bean and myCoach == anotherCoach? " + (myCoach == anotherCoach));
+
     }
 
     @GetMapping("/getdailyworkout")
     public String getDailyWorker(){
         return myCoach.dailyPractice();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "answer is " + (this.myCoach == this.anotherCoach);
     }
 }
